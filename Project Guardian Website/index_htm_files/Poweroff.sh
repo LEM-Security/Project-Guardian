99ff9ee3dc8d5307bbf6cddba8de6febe79d745b
LEM Security LLC <40939532+LEM-Security@users.noreply.github.com>
2018-07-25 18:52:10 -0700

    Website content

---
+++ Project Guardian Website/index_htm_files/Poweroff.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#Script Created by LEM Security for "Project Guardian"
+#6/24/2018
+#Used to power off the device from the Web UI
+
+sudo poweroff 
---
+++ Project Guardian Website/index_htm_files/Reboot.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#Script Created by LEM Security for "Project Guardian"
+#6/24/2018
+#Used to reboot the device from the Web UI
+
+sudo reboot 
